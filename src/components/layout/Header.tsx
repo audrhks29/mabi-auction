cc7fdc972dace8c5300b4dcdfcb97af23b6c2940
audrhks29 <audrhks29@gmail.com>
2024-06-11 23:42:34 +0900

    Fix : Header 임시로고

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,11 @@ export default function Header() {
   return (
     <nav className="flex h-14 justify-between items-center">
       <div className="flex h-14">
-        <h1 className="w-16">로고</h1>
+        <h1 className="w-16 cursor-pointer">
+          <Link href="/" className="flex w-full h-full justify-center items-center">
+            로고
+          </Link>
+        </h1>
 
         <ul className="h-full">
           <li className="w-[200px] text-center h-full hover:bg-muted cursor-pointer">

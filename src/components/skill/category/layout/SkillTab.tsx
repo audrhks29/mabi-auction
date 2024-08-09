5d7c69d67da781f95842dfb60e3d468ac4e12512
audrhks29 <audrhks29@gmail.com>
2024-08-09 17:33:36 +0900

    Fix: 스킬 탭 레이아웃 수정

--- src/components/skill/category/layout/SkillTab.tsx
+++ src/components/skill/category/layout/SkillTab.tsx
@@ -1,48 +1,52 @@
 "use client";
 
 import Link from "next/link";
-import { useParams } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import talentLists from "@/assets/skill/talent.json";
 import categoryLists from "@/assets/skill/skillCategory.json";
+import { useParams } from "next/navigation";
 
 export default function SkillTab() {
+  const params = useParams();
+
   return (
-    <Tabs defaultValue="category" className="mb-3">
-      <TabsList>
-        <TabsTrigger value="category">분류별</TabsTrigger>
-        <TabsTrigger value="talent">재능별</TabsTrigger>
-      </TabsList>
+    Object.keys(params).length !== 0 && (
+      <Tabs defaultValue="category" className="mb-3">
+        <TabsList>
+          <TabsTrigger value="category">분류별</TabsTrigger>
+          <TabsTrigger value="talent">재능별</TabsTrigger>
+        </TabsList>
 
-      <TabsContent value="category" className="border p-3 rounded-sm">
-        <ul className="grid grid-cols-11 gap-2 text-center text-[14px]">
-          {categoryLists.map(category => {
-            return (
-              <li key={category.id}>
-                <Link href={category.link ? `/skill/category/${category.link}` : ""} className="hover:underline">
-                  {category.category}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </TabsContent>
+        <TabsContent value="category" className="border p-3 rounded-sm">
+          <ul className="grid grid-cols-11 gap-2 text-center text-[14px]">
+            {categoryLists.map(category => {
+              return (
+                <li key={category.id}>
+                  <Link href={category.link ? `/skill/category/${category.link}` : ""} className="hover:underline">
+                    {category.category}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </TabsContent>
 
-      <TabsContent value="talent" className="border p-3 rounded-sm">
-        <ul className="grid grid-cols-11 gap-2 text-center text-[14px]">
-          {talentLists.map(talent => {
-            return (
-              <li key={talent.id}>
-                <Link href={talent.link ? `/skill/talent/${talent.link}` : ""} className="hover:underline">
-                  {talent.talent}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </TabsContent>
-    </Tabs>
+        <TabsContent value="talent" className="border p-3 rounded-sm">
+          <ul className="grid grid-cols-11 gap-2 text-center text-[14px]">
+            {talentLists.map(talent => {
+              return (
+                <li key={talent.id}>
+                  <Link href={talent.link ? `/skill/talent/${talent.link}` : ""} className="hover:underline">
+                    {talent.talent}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </TabsContent>
+      </Tabs>
+    )
   );
 }

63110e06179f8fa9f418958d6ab8afa8b4b4b7b0
audrhks29 <audrhks29@gmail.com>
2024-10-20 03:02:03 +0900

    docs: 미사용 파일 삭제

--- src/utils/loadSkillLists.ts
+++
@@ -1,8 +0,0 @@
-import skillLists from "@/assets/skill/human/skill.json";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-
-export const loadSkillLists = (params: Params) => {
-  return skillLists.filter(skillList =>
-    params.type === "category" ? skillList.category === params.tab : skillList.talent.includes(params.tab as string),
-  );
-};
--- src/utils/stats/statsTitle.ts
+++
@@ -1,3 +0,0 @@
-type StatsTitleTypes = "hp" | "mp" | "sp" | "str" | "dex" | "int" | "will" | "luck";
-
-export const statsTitle: StatsTitleTypes[] = ["hp", "mp", "sp", "str", "dex", "int", "will", "luck"];

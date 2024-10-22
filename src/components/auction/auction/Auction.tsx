db821b94df76e179b90aa5cb901ecc3370ad8df5
audrhks29 <audrhks29@gmail.com>
2024-10-22 10:21:20 +0900

    Code: fetchItemLists 함수 모듈화

--- src/components/auction/auction/Auction.tsx
+++ src/components/auction/auction/Auction.tsx
@@ -10,6 +10,8 @@ import SearchBox from "./SearchBox";
 
 import NonData from "@/components/shared/NonData";
 
+import { fetchItemLists } from "@/services/auctionApi";
+
 export default function Auction() {
   const { handleSubmit, register, getValues, setValue } = useForm<AuctionSearchFormTypes>();
   const [category, setCategory] = useState<ItemCategoryStateType>({
@@ -17,42 +19,9 @@ export default function Auction() {
     detailCategory: null,
   });
 
-  const fetchItemLists = async () => {
-    const inputText = getValues().inputText;
-
-    if (inputText !== "" || category.detailCategory) {
-      let urlString;
-      if (category.detailCategory) {
-        // 카테고리 클릭시 검색
-        const detailCategoryEncoded = encodeURI(category.detailCategory);
-        urlString = `https://open.api.nexon.com/mabinogi/v1/auction/list?auction_item_category=${detailCategoryEncoded}`;
-      } else {
-        // 검색어 입력시 검색
-        const inputTextEncoded = encodeURI(inputText);
-        urlString = `https://open.api.nexon.com/mabinogi/v1/auction/list?item_name=${inputTextEncoded}`;
-      }
-
-      const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-      try {
-        const headers: HeadersInit = API_KEY ? { "x-nxopen-api-key": API_KEY } : {};
-
-        const res = await fetch(urlString, {
-          headers,
-        });
-
-        const resData = await res.json();
-        return resData;
-      } catch (error) {
-        console.error("An unexpected error happened:", error);
-      }
-    } else {
-      return [];
-    }
-  };
-
   const { data, refetch, isFetching } = useQuery({
     queryKey: ["itemLists", getValues().inputText || category.detailCategory],
-    queryFn: fetchItemLists,
+    queryFn: () => fetchItemLists(getValues, category),
     select: data => {
       return data.auction_item;
     },

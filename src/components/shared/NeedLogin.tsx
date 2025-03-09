f61c3ff2c38df1a69c322f206a180527caac8f3d
audrhks29 <audrhks29@gmail.com>
2025-03-09 22:03:06 +0900

    Fix: NeedLogin 컴포넌트 수정

--- src/components/shared/NeedLogin.tsx
+++ src/components/shared/NeedLogin.tsx
@@ -1,12 +1,10 @@
+import { Info } from "lucide-react";
+
 export default function NeedLogin() {
   return (
-    <div className="relative flex justify-center items-center min-h-[150px]">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-[14px]">
-        로그인이 필요합니다.
-      </div>
-      <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-card to-primary">
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-primary-foreground rounded-full"></div>
-      </div>
+    <div className="flex justify-center items-center flex-col gap-3 h-56 border border-base-200">
+      <Info className="w-11 h-11" />
+      <span>로그인이 필요합니다.</span>
     </div>
   );
 }
